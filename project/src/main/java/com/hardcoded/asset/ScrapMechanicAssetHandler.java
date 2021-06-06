b62c6f17477a63bf60799433317af800fc5d7fad
Kariaro <25615845+Kariaro@users.noreply.github.com>
2021-06-06 22:02:04 +0200

    Release 0.0.4 [skip ci]

--- project/src/main/java/com/hardcoded/asset/ScrapMechanicAssetHandler.java
+++ project/src/main/java/com/hardcoded/asset/ScrapMechanicAssetHandler.java
@@ -15,7 +15,7 @@ import com.hardcoded.logger.Log.Level;
  * @since v0.1
  */
 public final class ScrapMechanicAssetHandler {
-	public static final String VERSION_TEST = "0.0.3";
+	public static final String VERSION_TEST = "0.0.4";
 	
 	private static final Log LOGGER = Log.getLogger();
 	
