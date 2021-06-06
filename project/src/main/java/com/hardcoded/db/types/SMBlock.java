1d66ea68d77e48f757e3685abe9c7046cbdc5d64
Kariaro <25615845+Kariaro@users.noreply.github.com>
2021-06-06 22:11:40 +0200

    Update file versions 0.0.8 [skip ci]

--- project/src/main/java/com/hardcoded/db/types/SMBlock.java
+++ project/src/main/java/com/hardcoded/db/types/SMBlock.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  * @since v0.1
  */
 public class SMBlock {
-	public static final String VERSION_TEST = "0.0.7";
+	public static final String VERSION_TEST = "0.0.8";
 	
 	public UUID uuid;
 	public String name;
