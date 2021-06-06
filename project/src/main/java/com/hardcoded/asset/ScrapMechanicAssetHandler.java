86d2f1f4e900fa312f1d9e04a1c5e82442fea1ad
Kariaro <25615845+Kariaro@users.noreply.github.com>
2021-06-06 22:14:52 +0200

    Update file versions 0.0.9 [skip ci]

--- project/src/main/java/com/hardcoded/asset/ScrapMechanicAssetHandler.java
+++ project/src/main/java/com/hardcoded/asset/ScrapMechanicAssetHandler.java
@@ -15,7 +15,7 @@ import com.hardcoded.logger.Log.Level;
  * @since v0.1
  */
 public final class ScrapMechanicAssetHandler {
-	public static final String VERSION_TEST = "0.0.8";
+	public static final String VERSION_TEST = "0.0.9";
 	
 	private static final Log LOGGER = Log.getLogger();
 	
--- project/src/main/java/com/hardcoded/db/types/SMBlock.java
+++ project/src/main/java/com/hardcoded/db/types/SMBlock.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  * @since v0.1
  */
 public class SMBlock {
-	public static final String VERSION_TEST = "0.0.8";
+	public static final String VERSION_TEST = "0.0.9";
 	
 	public UUID uuid;
 	public String name;
