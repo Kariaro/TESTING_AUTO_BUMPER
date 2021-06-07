f2911aaa8e1b311580ea6ccb10abb34471f7a209
Kariaro <25615845+Kariaro@users.noreply.github.com>
2021-06-07 18:09:33 +0200

    Update @auto-bumper 0.0.13 [skip ci]

--- res/test/test_1.js
+++ res/test/test_1.js
@@ -1,4 +1,4 @@
-// @since v0.0.12-SNAPSHOT
-// @since v0.0.12-SNAPSHOT
-// @since v0.0.12-SNAPSHOT
-// @since v0.0.12-SNAPSHOT
+// @since v0.0.13-SNAPSHOT
+// @since v0.0.13-SNAPSHOT
+// @since v0.0.13-SNAPSHOT
+// @since v0.0.13-SNAPSHOT
