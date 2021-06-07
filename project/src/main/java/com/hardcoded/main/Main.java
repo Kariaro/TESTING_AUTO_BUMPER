6e680d250c5feab337a5fad5f69715c82336e483
Kariaro <25615845+Kariaro@users.noreply.github.com>
2021-06-07 11:35:29 +0200

    Removed separate package test

--- project/src/main/java/com/hardcoded/main/Main.java
+++ project/src/main/java/com/hardcoded/main/Main.java
@@ -1,39 +1,39 @@
 package com.hardcoded.main;
 
-import com.hardcoded.game.World;
+import com.hardcoded.asset.ScrapMechanicAssetHandler;
 import com.hardcoded.logger.Log;
-import com.hardcoded.sm.api.WorldHeader;
-import com.hardcoded.sm.api.WorldHeader.Mod;
 
 public class Main {
 	private static final Log LOGGER = Log.getLogger();
 	
 	public static void main(String[] args) throws Exception {
-		//new Main();
+		// Init
+		ScrapMechanicAssetHandler.getAllAssets();
+		LOGGER.debug("testing: %s", "abcd");
 	}
 	
 	public Main() {
-		World world;
-		try {
-			//world = World.loadWorldFromAppdata("TestingSQLite.db");
-			world = World.loadWorldFromAppdata("Survival/Amazing World.db");
-		} catch(Exception e) {
-			LOGGER.error("Failed to load world file");
-			LOGGER.throwing(e);
-			return;
-		}
-		
-		WorldHeader game = world.getWorldHeader();
-		LOGGER.info("Version: %d", game.getVersion());
-		LOGGER.info("Flags: %d", game.getFlags());
-		LOGGER.info("Seed: %d", game.getSeed());
-		LOGGER.info("Tick: %d", game.getGameTick());
-		
-		LOGGER.info("Mods:");
-		for(Mod mod : game.getMods()) {
-			LOGGER.info("    Mod: %s", mod);
-			LOGGER.info("      localId: %d", mod.localId);
-			LOGGER.info("      fileId : %d", mod.fileId);
-		}
+//		World world;
+//		try {
+//			//world = World.loadWorldFromAppdata("TestingSQLite.db");
+//			world = World.loadWorldFromAppdata("Survival/Amazing World.db");
+//		} catch(Exception e) {
+//			LOGGER.error("Failed to load world file");
+//			LOGGER.throwing(e);
+//			return;
+//		}
+//		
+//		WorldHeader game = world.getWorldHeader();
+//		LOGGER.info("Version: %d", game.getVersion());
+//		LOGGER.info("Flags: %d", game.getFlags());
+//		LOGGER.info("Seed: %d", game.getSeed());
+//		LOGGER.info("Tick: %d", game.getGameTick());
+//		
+//		LOGGER.info("Mods:");
+//		for(Mod mod : game.getMods()) {
+//			LOGGER.info("    Mod: %s", mod);
+//			LOGGER.info("      localId: %d", mod.localId);
+//			LOGGER.info("      fileId : %d", mod.fileId);
+//		}
 	}
 }
--- project/src/main/java/com/hardcoded/world/utils/ShapeUtils.java
+++ project/src/main/java/com/hardcoded/world/utils/ShapeUtils.java
@@ -1,89 +1,82 @@
 package com.hardcoded.world.utils;
 
-import org.joml.Matrix4f;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
-import org.joml.Vector4f;
-
-import com.hardcoded.asset.ScrapMechanicAssetHandler;
-import com.hardcoded.db.types.SMPart;
-import com.hardcoded.sm.objects.BodyList.ChildShape;
-import com.hardcoded.sm.objects.BodyList.RigidBody;
 
 public class ShapeUtils {
-	public static Bounds3D getBoundingBox(RigidBody body) {
-		Vector3f posi = new Vector3f(Float.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE);
-		Vector3f size = new Vector3f(-Float.MAX_VALUE, -Float.MAX_VALUE, -Float.MAX_VALUE);
-		
-		for(ChildShape shape : body.shapes) {
-			if(shape.shapeType == 0x1f) {
-				float x = shape.xPos;
-				float y = shape.yPos;
-				float z = shape.zPos;
-				float xs = x + shape.xSize;
-				float ys = y + shape.ySize;
-				float zs = z + shape.zSize;
-				
-				if(x < posi.x) posi.x = x;
-				if(y < posi.y) posi.y = y;
-				if(z < posi.z) posi.z = z;
-				if(xs > size.x) size.x = xs;
-				if(ys > size.y) size.y = ys;
-				if(zs > size.z) size.z = zs;
-				continue;
-			}
-			
-			// TODO: Get bounding box of all parts. This should be cached
-			SMPart part = ScrapMechanicAssetHandler.getPart(shape.uuid);
-			if(part != null) {
-				float x = shape.xPos;
-				float y = shape.yPos;
-				float z = shape.zPos;
-				Vector4f zero = new Vector4f();
-				Matrix4f rot = new Matrix4f(PartRotation.getRotationMultiplier(shape.partRotation));
-				BoxBounds bounds = part.getBounds();
-				rot.setTranslation(0, 0, 0);
-				if(bounds != null) {
-					rot.translate(
-						bounds.getWidth(),
-						bounds.getHeight(),
-						bounds.getDepth()
-					);
-				}
-				zero.mul(rot);
-				
-				float xs = x + zero.x;
-				float ys = y + zero.y;
-				float zs = z + zero.z;
-				if(xs < x) {
-					float tmp = xs; xs = x; x = tmp;
-				}
-				if(ys < y) {
-					float tmp = ys; ys = y; y = tmp;
-				}
-				if(zs < z) {
-					float tmp = zs; zs = z; z = tmp;
-				}
-
-				if(x < posi.x) posi.x = x;
-				if(y < posi.y) posi.y = y;
-				if(z < posi.z) posi.z = z;
-				if(xs > size.x) size.x = xs;
-				if(ys > size.y) size.y = ys;
-				if(zs > size.z) size.z = zs;
-			}
-		}
-		
-		Bounds3D bounds = new Bounds3D(posi, size);
-		bounds.xMax *= 0.25f;
-		bounds.xMin *= 0.25f;
-		bounds.yMax *= 0.25f;
-		bounds.yMin *= 0.25f;
-		bounds.zMax *= 0.25f;
-		bounds.zMin *= 0.25f;
-		
-		return bounds;
-	}
+//	public static Bounds3D getBoundingBox(RigidBody body) {
+//		Vector3f posi = new Vector3f(Float.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE);
+//		Vector3f size = new Vector3f(-Float.MAX_VALUE, -Float.MAX_VALUE, -Float.MAX_VALUE);
+//		
+//		for(ChildShape shape : body.shapes) {
+//			if(shape.shapeType == 0x1f) {
+//				float x = shape.xPos;
+//				float y = shape.yPos;
+//				float z = shape.zPos;
+//				float xs = x + shape.xSize;
+//				float ys = y + shape.ySize;
+//				float zs = z + shape.zSize;
+//				
+//				if(x < posi.x) posi.x = x;
+//				if(y < posi.y) posi.y = y;
+//				if(z < posi.z) posi.z = z;
+//				if(xs > size.x) size.x = xs;
+//				if(ys > size.y) size.y = ys;
+//				if(zs > size.z) size.z = zs;
+//				continue;
+//			}
+//			
+//			// TODO: Get bounding box of all parts. This should be cached
+//			SMPart part = ScrapMechanicAssetHandler.getPart(shape.uuid);
+//			if(part != null) {
+//				float x = shape.xPos;
+//				float y = shape.yPos;
+//				float z = shape.zPos;
+//				Vector4f zero = new Vector4f();
+//				Matrix4f rot = new Matrix4f(PartRotation.getRotationMultiplier(shape.partRotation));
+//				BoxBounds bounds = part.getBounds();
+//				rot.setTranslation(0, 0, 0);
+//				if(bounds != null) {
+//					rot.translate(
+//						bounds.getWidth(),
+//						bounds.getHeight(),
+//						bounds.getDepth()
+//					);
+//				}
+//				zero.mul(rot);
+//				
+//				float xs = x + zero.x;
+//				float ys = y + zero.y;
+//				float zs = z + zero.z;
+//				if(xs < x) {
+//					float tmp = xs; xs = x; x = tmp;
+//				}
+//				if(ys < y) {
+//					float tmp = ys; ys = y; y = tmp;
+//				}
+//				if(zs < z) {
+//					float tmp = zs; zs = z; z = tmp;
+//				}
+//
+//				if(x < posi.x) posi.x = x;
+//				if(y < posi.y) posi.y = y;
+//				if(z < posi.z) posi.z = z;
+//				if(xs > size.x) size.x = xs;
+//				if(ys > size.y) size.y = ys;
+//				if(zs > size.z) size.z = zs;
+//			}
+//		}
+//		
+//		Bounds3D bounds = new Bounds3D(posi, size);
+//		bounds.xMax *= 0.25f;
+//		bounds.xMin *= 0.25f;
+//		bounds.yMax *= 0.25f;
+//		bounds.yMin *= 0.25f;
+//		bounds.zMax *= 0.25f;
+//		bounds.zMin *= 0.25f;
+//		
+//		return bounds;	
+//	}
 	
 	public static class Bounds3D {
 		public float xMin, xMax;
