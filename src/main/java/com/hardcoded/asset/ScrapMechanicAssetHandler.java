47159c8070c449785a8b9f9c17d9960477c5c670
HardCoded <runner@fv-az28-142.ycjfjujedjyu5lezsnvlijijec.cx.internal.cloudapp.net>
2021-06-07 11:16:31 +0000

    Update -bumper 0.0.11 [skip ci]

--- src/main/java/com/hardcoded/asset/ScrapMechanicAssetHandler.java
+++ src/main/java/com/hardcoded/asset/ScrapMechanicAssetHandler.java
@@ -15,7 +15,7 @@ import com.hardcoded.logger.Log.Level;
  * @since v0.1
  */
 public final class ScrapMechanicAssetHandler {
-	public static final String VERSION_TEST = "0.0.10";
+	public static final String VERSION_TEST = "0.0.11";
 	
 	private static final Log LOGGER = Log.getLogger();
 	
